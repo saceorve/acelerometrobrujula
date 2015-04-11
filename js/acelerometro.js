138671dd20113db28edb7c7375c74389b21559a7
saceorve <sandyorozcov@hotmail.com>
2015-04-11 11:18:43 -0500

    11 Abril
    Prueba 2

--- js/acelerometro.js
+++ js/acelerometro.js
@@ -1,23 +1,24 @@
+//Acelerómetro
 var ac = {
     watchID: null,
-    satisfactorio: function(acc){
-     $('#acelerometro h2').html('X: '+acc.x+'<br>'+
-          'Y: '+acc.y+'<br>'+
-          'Z: '+acc.z);
-     }, 
-    error:  function(err){
-     alert('Error'+err.code);
-     },
-    opc: {frequency: 500}
+    satifactorio: function(acc){
+        $('#acelerometro h2').html('X: ' + acc.x + '<br>' +
+                                   'Y: ' + acc.y + '<br>' +
+                                   'Z: ' + acc.z);
+    },
+    error: function(err){
+        alert('Acelerómetro Error: '+err.code);
+    },
+    opc: { frequency: 500 },
     iniciar: function(){
-    If(ac.watchID == null){
-    ac.watchID = navigator.accelerometer.watchAcceleration(ac.satisfactorio,ac.error,ac.opc);
+        if(ac.watchID == null)
+            ac.watchID = navigator.accelerometer.watchAcceleration(ac.satifactorio,ac.error,ac.opc);
     },
     detener: function(){
-     if(ac.watchID){
-      navigator.accelerometer.clearWatch(watchID);
-ac.watchID = null;
-$('#acelerometro h2').html('Detenido');
-     }
+        if(ac.watchID){
+            navigator.accelerometer.clearWatch(ac.watchID);
+            ac.watchID = null;
+            $('#acelerometro h2').html('Detenido');
+        }
     }
 };
--- js/actions.js
+++ js/actions.js
@@ -6,20 +6,15 @@ var jQT = new $.jQTouch({
 var fn = {
     ready: function(){
         document.addEventListener('deviceready',fn.init,false);
-    }
+    },
     init: function(){
         $('#acelerometro ul.individual li').tap(fn.acTap);
-
     },
     acTap: function(){
         if($(this).index()==0)
-        {//iniciar
-            ac.iniciar();
-        }
+            ac.iniciar();//Iniciar
         else
-        {//detener
-            ac.detener();
-        }
+            ac.detener();//Detener
     }
 };
 $(fn.ready);
